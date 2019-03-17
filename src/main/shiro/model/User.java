b4a939d733291bdf1ace707fb31a08062f4ec2f3
yslu <469899431@qq.com>
2019-03-17 16:44:15 +0800

    Shiro 初步 jar包无法使用maven再用 done

---
+++ src/main/shiro/model/User.java
@@ -0,0 +1,23 @@
+package main.shiro.model;
+
+/**
+ * @Description: 用户
+ * @Author: LYS
+ * @Date: 2019/3/17 15:30
+ */
+public class User {
+    private String name;
+    private String password;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
